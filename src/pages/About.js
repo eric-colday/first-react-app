8d578926160c8c59185bfc07463380f7dbe3fdf8
Eric Noubissi <noubissi10@yahoo.fr>
2022-02-07 00:09:57 +0100

    Projet pays en react

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo/>
+            <h1>À propos</h1>
+            <br />
+            <p>
+                Deserunt in laborum exercitation excepteur officia 
+                nulla dolor officia consectetur qui. Eiusmod commodo 
+                velit non tempor dolore dolore eiusmod ex sit esse occaecat. 
+                Eu laborum ullamco pariatur enim elit sint anim laboris ea 
+                laborum consectetur amet.
+            </p>
+            <br />
+            <p>
+                Deserunt in laborum exercitation excepteur officia 
+                nulla dolor officia consectetur qui. Eiusmod commodo 
+                velit non tempor dolore dolore eiusmod ex sit esse occaecat. 
+                Eu laborum ullamco pariatur enim elit sint anim laboris ea 
+                laborum consectetur amet.
+            </p>
+        </div>
+    );
+};
+
+export default About;
